306075a494499329ca866cb48b1440e7330a7b46
huangshuai <huangshuai@qiyi.com>
2019-08-02 11:26:26 +0800

    服务注册到Eureka

--- src/main/java/com/hush/cloudweb/CloudWebApplication.java
+++ src/main/java/com/hush/cloudweb/CloudWebApplication.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @Author: huangshuai
  */
+
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudWebApplication {
 
